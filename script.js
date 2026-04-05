ff1470c0c00ce713163880d29d442a4d779ff5aa
arturbuilds <melnikarthur280912@gmail.com>
2026-04-05 19:19:18 +0300

    JS update change budget btc

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
   // переменные для цен
-  const price_btc = document.querySelector('#price-btc');
+  let price_btc = document.querySelector('#price-btc');
+
+  // переменные для вычитывания прибыли 
+  let change_btc = document.querySelector('#change-btc');
 
   async function loadBTCPrice() {
-    try {
-      const res = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
-      const data = await res.json();
+  try {
+    const resPrice = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+    const dataPrice = await resPrice.json();
+    const price = Number(dataPrice.price).toLocaleString('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    });
 
-      const price = Number(data.price).toLocaleString('en-US', {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      });
+    price_btc.textContent = price + ' USD';
 
-      price_btc.textContent = price + ' USD';
-    } catch (err) {
-      console.error('Ошибка загрузки цены BTC:', err);
-      price_btc.textContent = 'Error';
+    const res24h = await fetch('https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT');
+    const data24h = await res24h.json();
+    const changePercent = Number(data24h.priceChangePercent).toFixed(2);
+
+    const changeEl = change_btc;
+    changeEl.textContent = changePercent + '%';
+
+    if (changePercent >= 0) {
+      changeEl.classList.add('price-up');
+      changeEl.classList.remove('price-down');
+    } else {
+
+      changeEl.classList.add('price-down');
+      changeEl.classList.remove('price-up');
     }
+
+  } catch (err) {
+    console.error(err);
+    price_btc.textContent = 'Error';
+    change_btc.textContent = '';
   }
+}
 
   loadBTCPrice();
   setInterval(loadBTCPrice, 5000);
