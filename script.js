80c8ccaa9a8e0bcefa323926ea1ac6831686d44b
arturbuilds <melnikarthur280912@gmail.com>
2026-04-05 19:01:30 +0300

    New design

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.accordion-toggle');
-  const content = document.querySelector('.accordion-content');
-
-  // цены
-  let price_btc = document.querySelector('#price-btc');
-
-  toggle.addEventListener('click', () => {
-    toggle.classList.toggle('active');
-    content.classList.toggle('open');
-  });
+  // переменные для цен
+  const price_btc = document.querySelector('#price-btc');
 
   async function loadBTCPrice() {
     try {
-      const res = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
-      const data = await res.json()
+      const res = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+      const data = await res.json();
 
-      let price = Number(data.price).toLocaleString('en-US', {
+      const price = Number(data.price).toLocaleString('en-US', {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2
       });
 
-      price_btc.textContent = '$' + price + ' USD';
+      price_btc.textContent = price + ' USD';
     } catch (err) {
-      console.error(err);
+      console.error('Ошибка загрузки цены BTC:', err);
+      price_btc.textContent = 'Error';
     }
   }
 
